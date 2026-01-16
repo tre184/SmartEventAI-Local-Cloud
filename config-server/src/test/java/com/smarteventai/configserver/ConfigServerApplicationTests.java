645ef3e1fa52c22aebe09bbd5d3ca9ef34233b16
tre184 <74012437+tre184@users.noreply.github.com>
2026-01-16 06:17:56 +0100

    Update CI/CD v3

--- config-server/src/test/java/com/smarteventai/configserver/ConfigServerApplicationTests.java
+++ config-server/src/test/java/com/smarteventai/configserver/ConfigServerApplicationTests.java
@@ -1,13 +1,20 @@
 package com.smarteventai.configserver;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class ConfigServerApplicationTests {
 
+    @Autowired
+    private ApplicationContext context;
+
     @Test
     void contextLoads() {
+        assertThat(context).isNotNull();
     }
 
 }
--- gateway-service/src/test/java/com/smarteventai/gatewayservice/GatewayServiceApplicationTests.java
+++ gateway-service/src/test/java/com/smarteventai/gatewayservice/GatewayServiceApplicationTests.java
@@ -1,13 +1,20 @@
 package com.smarteventai.gatewayservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class GatewayServiceApplicationTests {
 
+    @Autowired
+    private ApplicationContext context;
+
     @Test
     void contextLoads() {
+        assertThat(context).isNotNull();
     }
 
 }
